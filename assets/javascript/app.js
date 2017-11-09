36a36eedcb27ef0271b74f04a1da392741e5385a
Rafal Kociolek <Rafal0933@hotmail.com>
2017-11-09 10:47:09 -0600

    Initial File Upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+/*var config = {
+    apiKey: "AIzaSyDILcPtZWSXU_1Q95f2eIbqZ75ifxkEEew",
+    authDomain: "fir-homework-3dc85.firebaseapp.com",
+    databaseURL: "https://fir-homework-3dc85.firebaseio.com",
+    storageBucket: "fir-homework-3dc85",
+    messagingSenderId: "473218229324"
+};
+
+firebase.initializeApp(config);*/
+
+$("#train-information-form").on("submit", function(event) {
+    event.preventDefault();
+    var trainName = $("#mySearch").val()
+    console.log(trainName);
+/*    var destination =
+    var firstTrainTime =
+    var frequency =*/
+});
