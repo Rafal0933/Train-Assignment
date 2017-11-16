fb994cb51fe88267f6c9868e3472724d85019242
Rafal Kociolek <Rafal0933@hotmail.com>
2017-11-15 20:17:46 -0600

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,80 +1,3 @@
-var state = [
-    {
-        trainName: "Trenton Express",
-        destination: "Trenton",
-        frequency: 25
-    },
-    {
-        trainName: "Oregon Trail",
-        destination: "Salem",
-        frequency: 3600
-    },
-    {
-        trainName: "Midnight Carriage",
-        destination: "Philadelphia",
-        frequency: 12
-    },
-    {
-        trainName: "Sing Sing Caravan",
-        destination: "Atlanta",
-        frequency: 45
-    },
-    {
-        trainName: "Boston Bus",
-        destination: "Boston",
-        frequency: 65
-    },
-    {
-        trainName: "California Caravan",
-        destination: "San Francisco",
-        frequency: 6000
-    },
-    {
-        trainName: "Analben's Train",
-        destination: "San Florida",
-        frequency: 25
-    },
-];
-
-
-minutesAway = [];
-nextArrival = [];
-
-function frequencyConversion() {
-
-    state.forEach(function(element) {
-var timeFrequency = (element.frequency);
-var firstTime = "03:00";
-var firstTimeConverted = moment(firstTime, "hh:mm");
-var currentTime = moment().format("hh:mm");
-var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm A");
-    minutesAway.push(tMinutesTillTrain);
-    nextArrival.push(nextTrain);
-    console.log(timeRemainer);
-    console.log(tMinutesTillTrain);
-    })
-};
-
-frequencyConversion();
-
-function renderTable () {
-
-    $("tbody").empty();
-    state.forEach(function(element) {
-        var tableRow = `
-                        <tr>
-                        <td>${element.trainName}</td>
-                        <td>${element.destination}</td>
-                        <td>${element.frequency}</td>
-                        <td>${nextArrival}</td>
-                        <td>${minutesAway}</td>
-                        </tr>
-                        `
-    $("tbody").append(tableRow);
-    })
-};
-renderTable();
 
 var config = {
     apiKey: "AIzaSyD3KbtttInL67l5FuC6WhIXxA-bwm3Fz-s",
@@ -84,38 +7,96 @@ var config = {
     messagingSenderId: "820750302472"
 };
 
+// Initizliaing firebase //
 firebase.initializeApp(config);
 
+// Create a variable to hold the firebase function //
 var database = firebase.database();
 
+// Create a variable to hold train name //
 var trainName = "";
 var destination = "";
-var trainStart = 0;
-var frequency = 0;
+var trainTime = "";
+
 
 $("#train-information-form").on("submit", function(event) {
     event.preventDefault();
 
+    // Take the value from train name form and trim to get rid of empty spaces //
     var trainName = $("#trainNameInput").val().trim();
+
+    // Take the value from destination in form and trim to get rid of empty spaces //
     var destination = $("#destinationInput").val().trim();
+
+    // Take value entered into train start time and trim to get rid of empty spaces //
     var trainStart = $("#traintimeInput").val().trim();
+
+    // Take value entered into frequency and trim to get rid of empty spaces //
     var frequency = $("#frequencyInput").val().trim();
 
+    // Format first train from military time into standard 12 hour //
+    var firstTime = moment(trainStart,"HHmm").format("hh:mm a");
+
+    // Moves time a year behind to make sure that it isn't ahead of the real time //
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+
+    // Takes the difference in time between first time and now into minutes//
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+    // Takes the difference in time and divides the remainder by frequency //
+    var nextTrainTime = diffTime % frequency;
+
+    // Subtracts the frequency from the next train time //
+    var tMinutesTillTrain = frequency - nextTrainTime;
+
+    // Adds the time remaining to current time and changes the format to hours and minutes //
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("h:mm A");
+
+    // Format the current time into standard //
+    var currentTime = moment().format("hh:mm a");
+
+    // Subtract first time from current time and formats it to standard //
+    var minutesAway = moment.utc(moment(firstTime,"HH:mm a").diff(moment(currentTime,"HH:mm a"))).format("HH:mm a");
+
+    // Convert minutes away into hour and minute format //
+    var minuteConversion = moment(minutesAway, "h:mm").format("h mm");
+
+
+// Push referenced variables into Firebase //
     database.ref().push({
 
-        Destination: destination,
-        Frequency: frequency,
+        // Enter train name value stored in trainName variable into Firebase //
         Name: trainName,
-        Start: trainStart,
+
+        // Enter destination name value stored in destination variable value into Firebase //
+        Destination: destination,
+
+        // Enter the frequency value stored in variable into Firebase //
+        Frequency: frequency,
+
+        // Enter the value stored in nextTrain variable into Firebase //
+        NextArrival: nextTrain,
+
+        // Enter the amount of time stored in tMinutesTillTrain variable into Firebase
+        MinutesAway: tMinutesTillTrain
     });
 });
 
+// When values (children) are added into Firebase, run function //
 database.ref().on("child_added", function(childSnapshot){
 
-    destinationOutput = childSnapshot.val().Destination;
-    frequencyOutput = childSnapshot.val().Frequency;
+    // Set variable to hold the value of Name in Firefox //
     trainNameOutput = childSnapshot.val().Name;
-    trainStartOutput = childSnapshot.val().Start;
+    // Set variable to hold the value of Destination in Firefox //
+    destinationOutput = childSnapshot.val().Destination;
+    // Set variable to hold the value of Frequency in Firefox //
+    frequencyOutput = childSnapshot.val().Frequency;
+    // Set variable to hold the value of NextArrival in Firefox //
+    nextTrainTimeOutput = childSnapshot.val().NextArrival;
+    // Set variable to hold the value of MinutesAway in Firefox //
+    tMinutesTillTrainOutput = childSnapshot.val().MinutesAway;
 
-    $("#current-schedule").append("<tr>" + "<td>" + trainNameOutput + "</td>" + "<td>" + destinationOutput + "</td>"+ "<td>" + frequencyOutput + "</td>" + "<td>" + frequencyOutput + "</td>" + "<td>" + frequencyOutput + "</tr>");
+    //Append values into table //
+    $("#current-schedule").append("<tr>" + "<td>" + trainNameOutput + "</td>" + "<td>" + destinationOutput + "</td>"+ "<td>" + frequencyOutput + " Minutes" + "</td>" + "<td>" + nextTrainTimeOutput + "</td>" + "<td>" + tMinutesTillTrainOutput + "</tr>");
 });
+
